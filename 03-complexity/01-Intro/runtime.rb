1d48ee56dfde858fe03dfd5616bf449c3292e85d
Jessica-Koch <jessicakoch136@icloud.com>
2018-03-30 22:13:36 -0700

    finish questions

---
+++ 03-complexity/01-Intro/runtime.rb
@@ -0,0 +1,14 @@
+def runtime(n)
+  puts "Collection of length #{n} would have a number of iterations of #{n}"
+end
+
+runtime(1)
+runtime(2)
+runtime(3)
+runtime(4)
+runtime(5)
+runtime(6)
+runtime(7)
+runtime(8)
+runtime(9)
+runtime(10)
